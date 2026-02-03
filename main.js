1d135b1964bc08189623ff090567c8757b6c4593
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2026-02-03 09:26:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ async function getQueryResults(query) {
   var page = await res.json();
   var data = [];
   for (var i of page.query.search) {
-    var dataRes = await fetch(`https://en.wikipedia.org/w/api.php?action=parse&page=${encodeURIComponent(i.title)}&prop=wikitext&format=json&origin=*`);
+    var dataRes = await fetch(`https://en.wikipedia.org/w/api.php?action=parse&page=${encodeURIComponent(i.title)}&prop=text&format=json&origin=*`);
     var pageData = await dataRes.json();
-    data.push(...pageData.parse.wikitext["*"].split(/[\s.,;:]/));
+    data.push(...pageData.parse.text["*"].split(/[\s.,;:]/));
   }
   var similarities = {};
   for (var i of data) {
